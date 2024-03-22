8857309259e0f39308d6f26d887110a2737f5d55
Chris <chris79gp@outlook.com>
2024-03-22 12:53:50 +0000

    test: add unit tests for passing invalid operands to calculator methods

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -23,6 +23,17 @@ describe("calculator", () => {
 	it.each(operations)("%s returns 0 if invoked with no arguments", (_, operation) => {
 		expect(operation()).toBe(0);
 	});
+	it.each(operations)("%s returns an error if invoked with an invalid operand", (_, operation) => {
+		expect(() => operation(12, "three")).toThrow(
+			"Error: one or both of 12 and three are invalid operands"
+		);
+		expect(() => operation("twelve", 3)).toThrow(
+			"Error: one or both of twelve and 3 are invalid operands"
+		);
+		expect(() => operation([4, 8], true)).toThrow(
+			"Error: one or both of 4,8 and true are invalid operands"
+		);
+	});
 	it("divide returns an error if b === 0", () => {
 		expect(() => calculator.divide(12, 0)).toThrow("Error: 0 is not a valid divisor");
 	});
