8376e962b739eaef64466834174a02f7b3c54436
Chris <chris79gp@outlook.com>
2024-03-22 11:53:06 +0000

    test: add unit tests for calculator methods

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,3 +1,15 @@
 import calculator from "../src/calculator";
 
-describe("calculator", () => {});
+describe("calculator", () => {
+	it("has methods for addition, subtraction, multiplication, and division", () => {
+		expect(calculator).toHaveProperty("add");
+		expect(calculator).toHaveProperty("subtract");
+		expect(calculator).toHaveProperty("multiply");
+		expect(calculator).toHaveProperty("divide");
+
+		expect(typeof calculator.add).toBe("function");
+		expect(typeof calculator.subtract).toBe("function");
+		expect(typeof calculator.multiply).toBe("function");
+		expect(typeof calculator.divide).toBe("function");
+	});
+});
--- src/calculator.js
+++ src/calculator.js
@@ -1,9 +1,11 @@
 // Write a calculator object that contains functions for the basic operations: add, subtract, divide, and multiply.
 // Each of these functions should take two numbers and return the correct calculation.
 
-export default calculator = {
+const calculator = {
 	add: (a, b) => {},
 	subtract: (a, b) => {},
-	multiple: (a, b) => {},
+	multiply: (a, b) => {},
 	divide: (a, b) => {},
 };
+
+export default calculator;
