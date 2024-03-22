8adf131c1ea096c1be61f993f0d5c1550ff524bf
Chris <chris79gp@outlook.com>
2024-03-22 12:54:04 +0000

    feat: passing invalid operands to calculator methods will throw an error

--- src/calculator.js
+++ src/calculator.js
@@ -2,10 +2,28 @@
 // Each of these functions should take two numbers and return the correct calculation.
 
 const calculator = {
-	add: (a = 0, b = 0) => a + b,
-	subtract: (a = 0, b = 0) => a - b,
-	multiply: (a = 0, b = 1) => a * b,
+	add: (a = 0, b = 0) => {
+		if (typeof a !== "number" || typeof b !== "number") {
+			throw new Error(`Error: one or both of ${a} and ${b} are invalid operands`);
+		}
+		return a + b;
+	},
+	subtract: (a = 0, b = 0) => {
+		if (typeof a !== "number" || typeof b !== "number") {
+			throw new Error(`Error: one or both of ${a} and ${b} are invalid operands`);
+		}
+		return a - b;
+	},
+	multiply: (a = 0, b = 1) => {
+		if (typeof a !== "number" || typeof b !== "number") {
+			throw new Error(`Error: one or both of ${a} and ${b} are invalid operands`);
+		}
+		return a * b;
+	},
 	divide: (a = 0, b = 1) => {
+		if (typeof a !== "number" || typeof b !== "number") {
+			throw new Error(`Error: one or both of ${a} and ${b} are invalid operands`);
+		}
 		if (b === 0) {
 			throw new Error("Error: 0 is not a valid divisor");
 		}
