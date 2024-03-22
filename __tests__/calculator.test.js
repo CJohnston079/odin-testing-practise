ab934f0ebc52de8319189546ae4ae7ca388e5aad
Chris <chris79gp@outlook.com>
2024-03-22 12:28:46 +0000

    test: add unit tests for calculator operations to return 0 if invoked with no arguments

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -20,6 +20,9 @@ describe("calculator", () => {
 	it.each(operations)("%s returns the same number if only passed one number", (_, operation) => {
 		expect(operation(12)).toBe(12);
 	});
+	it.each(operations)("%s returns 0 if invoked with no arguments", (_, operation) => {
+		expect(operation()).toBe(0);
+	});
 	it("divide returns an error if b === 0", () => {
 		expect(() => calculator.divide(12, 0)).toThrow("Error: 0 is not a valid divisor");
 	});
