8a7ddde20dfdd2d8abd27054913ab0d57f625956
Chris <chris79gp@outlook.com>
2024-03-23 17:30:46 +0000

    test: add unit tests for caesarCipher

--- __tests__/caesarCipher.test.js
+++ __tests__/caesarCipher.test.js
@@ -1,3 +1,11 @@
 import caesarCipher from "../src/caesarCipher";
 
-describe("caesarCipher", () => {});
+describe("caesarCipher", () => {
+	it("returns a string", () => {
+		expect(typeof caesarCipher("Hello World!", 2)).toBe("string");
+	});
+	it("returns the input string if shiftFactor is 0 or undefined", () => {
+		expect(caesarCipher("Hello World!", 0)).toBe("Hello World!");
+		expect(caesarCipher("Hello World!")).toBe("Hello World!");
+	});
+});
