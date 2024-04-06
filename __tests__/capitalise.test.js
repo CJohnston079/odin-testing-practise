9f6b7718f4d70bd51adadffcaecf5702fd9ecbd8
Chris <chris79gp@outlook.com>
2024-04-06 21:00:27 +0100

    test: add testing suite for capitalise.js

--- __tests__/capitalise.test.js
+++ __tests__/capitalise.test.js
@@ -1,3 +1,19 @@
 import capitalise from "../src/capitalise";
 
-describe("capitalise", () => {});
+describe("capitalise", () => {
+	it("returns a string", () => {
+		expect(typeof capitalise("test")).toBe("string");
+	});
+	it("returns an empty string if invoked without an argument", () => {
+		expect(capitalise()).toBe("");
+	});
+	it("returns the same string is first character is already capitalised", () => {
+		expect(capitalise("Test")).toBe("Test");
+	});
+	it("returns a capitalised string when passed a lower case string", () => {
+		expect(capitalise("test")).toBe("Test");
+	});
+	it("returns a capitalised lower case string when passes a mixed case string", () => {
+		expect(capitalise("tEsT")).toBe("TEsT");
+	});
+});
