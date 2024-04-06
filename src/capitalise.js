238caef98fe7a378438620f405ec03b43aac9837
Chris <chris79gp@outlook.com>
2024-04-06 21:01:49 +0100

    feat: add capitalise() takes a string parameter returns a string capitalises the first letter of the string if not already capitalised leaves the remainder of the string with original casing returns an empty string if invoked without an argument

--- src/capitalise.js
+++ src/capitalise.js
@@ -1,5 +1,7 @@
 // Write a capitalize function that takes a string and returns it with the first character capitalized.
 
-export default capitalise = function (str) {
-	return;
+export default capitalise = function (str = "") {
+	const capitalisedStr = str.charAt(0).toUpperCase() + str.slice(1);
+
+	return capitalisedStr;
 };
