784f155a88d31b5807cbdd9adfd3196f0ade2de3
Chris <chris79gp@outlook.com>
2024-04-07 21:41:07 +0100

    feat: add reverseString() to reverseString.js takes a string as an argument, which defaults to an empty string returns an empty string if invoked with an empty string or with no argument returns a string with the characters in reverse order

--- src/reverseString.js
+++ src/reverseString.js
@@ -1,5 +1,7 @@
 // Write a reverseString function that takes a string and returns it reversed.
 
-export default reverseString = function (str) {
-	return;
+export default reverseString = function (str = "") {
+	const reversedStr = str.split("").reverse().join("");
+
+	return reversedStr;
 };
