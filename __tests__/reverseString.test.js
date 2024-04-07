f07f57d88e402f4bc55af10daa28d2bb5739b3b7
Chris <chris79gp@outlook.com>
2024-04-07 21:39:52 +0100

    test: add unit tests for reverseString.js

--- __tests__/reverseString.test.js
+++ __tests__/reverseString.test.js
@@ -1,3 +1,22 @@
 import reverseString from "../src/reverseString";
 
-describe("reverseString", () => {});
+describe("reverseString", () => {
+	it("returns a string", () => {
+		expect(typeof reverseString("test")).toBe("string");
+	});
+	it("returns an empty string if invoked with no argument", () => {
+		expect(reverseString()).toBe("");
+	});
+	it("returns an empty strng if passed an empty string", () => {
+		expect(reverseString("")).toBe("");
+	});
+	it("returns the same string if passed a palindrome", () => {
+		expect(reverseString("tacocat")).toBe("tacocat");
+	});
+	it("reverses an even length string", () => {
+		expect(reverseString("test")).toBe("tset");
+	});
+	it("reverses an odd length string", () => {
+		expect(reverseString("testing")).toBe("gnitset");
+	});
+});
