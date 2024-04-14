59ca2c429867e9e504b9d7be9a3bca6076db225e
Chris <chris79gp@outlook.com>
2024-04-14 20:21:07 +0100

    test: add unit tests for analyseArray.js

--- __tests__/analyseArray.test.js
+++ __tests__/analyseArray.test.js
@@ -1,3 +1,27 @@
 import analyseArray from "../src/analyseArray";
 
-describe("analyseArray", () => {});
+describe("analyseArray", () => {
+	const result = analyseArray([1, 8, 3, 4, 2, 6]);
+
+	it("returns an object", () => {
+		expect(typeof result).toBe("object");
+	});
+	it("returnsa na object with properties average, min, max and length", () => {
+		expect(result).toHaveProperty("average");
+		expect(result).toHaveProperty("min");
+		expect(result).toHaveProperty("max");
+		expect(result).toHaveProperty("length");
+	});
+	it("returns the average of an array of numbers in the average property", () => {
+		expect(result.average).toBe(4);
+	});
+	it("returns the smallest number of an array of numbers in the min property", () => {
+		expect(result.min).toBe(1);
+	});
+	it("returns the largest number of an array of numbers in the max property", () => {
+		expect(result.max).toBe(8);
+	});
+	it("returns the length of the array in the length property", () => {
+		expect(result.length).toBe(6);
+	});
+});
